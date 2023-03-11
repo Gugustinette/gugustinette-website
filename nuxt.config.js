0c096620099d8c70bf1ba0d9766f43e256399c51
Gugustinette <mercier.augustin@outlook.fr>
2023-03-11 19:36:28 +0100

    Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'gugustinette-website',
+    title: 'Gugustinette Website',
     htmlAttrs: {
       lang: 'en',
     },
